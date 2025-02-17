319e344f5abaaf17fc4246249d2363d9718220fc
razvan.gaston <razvan.gaston@avios.com>
2025-02-17 11:13:21 +0200

    balance tracker init project

---
+++ src/main/java/raz/balance_tracker/BalanceTrackerApplication.java
@@ -0,0 +1,13 @@
+package raz.balance_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BalanceTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BalanceTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/raz/balance_tracker/BalanceTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package raz.balance_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BalanceTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
